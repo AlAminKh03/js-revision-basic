1d146df6774a042594563f5057cb2a5801f69d36
AlAminKh03 <alaminkhan6203@gmail.com>
2022-07-23 23:32:50 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,7 @@
+// Fixed values are literals values examples like 
+// 
+// variables is something that we use to store something like var , let , const 
+
+// console.log("2 plus 2 is equal 4 " + 2 + 2)
+
+console.log(12 + 3 + " is equal fifteen")
