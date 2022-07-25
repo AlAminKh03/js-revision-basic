d8acb4a11c25649d68059b1d8a611a771c28b46d
AlAminKh03 <alaminkhan6203@gmail.com>
2022-07-25 22:25:45 +0600

    array methods added

--- test.js
+++ test.js
@@ -4,4 +4,34 @@
 
 // console.log("2 plus 2 is equal 4 " + 2 + 2)
 
-console.log(12 + 3 + " is equal fifteen")
+// console.log(12 + 3 + " is equal fifteen")
+
+
+// function myFunction(a, b) {
+//     return a + b
+// }
+// myFunction(3, 4);
+
+
+
+
+//    ARRAY METHODS 
+const pets = ['cat', 'dog', 'hen', 'duck']
+// pop মেথড এরে এর শেষ ইলিমেন্ট কে বের করে দেয় 
+// pets.pop()
+// push () method insert an elemnt by the last in an array 
+// pets.push('monkey')
+// shift ( method removes element by first from an array )
+// pets.shift()
+// unshift medod add elements by first in a array 
+// pets.unshift('rat', 'human')
+/*splice method can add one or mire elements anywhere in the array ,
+it has two digit of number to work with and any string or value you wantto add . 
+first digit for which index it should statr and second one for from there how many elements it should delete */
+/*by splice method you can simply delete as many as you want just do not 
+add any value just add index number as your wish */
+// pets.splice(1, 2, 'ant', 'peigon')
+// By splice method we can store the deletinfg data like the below code 
+
+// let noPetInBd = pets.splice(0, 2)
+// console.log(noPetInBd)
