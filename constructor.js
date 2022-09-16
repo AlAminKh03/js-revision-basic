34271c281abdb8ec0facaa2688152fed5693fef8
AlAminKh03 <alaminkhan6203@gmail.com>
2022-09-17 00:55:20 +0600

    class added

--- constructor.js
+++ constructor.js
@@ -12,3 +12,7 @@ const alAmin = new SmartPhone("Samsung", "Note 10", 2022)
 
 const enam = new SmartPhone("Realme", "narzo 20", 2022)
 console.log(enam.makeCall("01737468327"))
+
+// ১, this কি ওয়ার্ড তার নিজস্ব object কে রেফার করে
+// ২, সাধারনত এটি উইন্ডো object কে রেফার করে
+// এটি একটি ফাংশন constructor মেথড 
